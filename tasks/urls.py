fd15b8925592860f2cb692b1d3d41035d2936aa2
ahmadx16 <ahmadx16@hotmail.com>
2020-08-07 20:56:20 +0500

    initial task add done

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'tasks'
 
 urlpatterns = [
     # /tasks/
-    path("", views.index, name="index")
+    path('', views.index, name='index')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -3,8 +3,16 @@ from .models import Task
 
 
 def index(request):
+    # adding new tasks
+    if request.method =='POST':
+        task = Task (detail=request.POST['newtask'])
+        task.save()
+    
     all_tasks = Task.objects.all()
     context = {
         "all_tasks": all_tasks
     }
+
+    
+        
     return render(request, 'tasks/index.html',context)
