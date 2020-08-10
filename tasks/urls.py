26cb65d41cb661a369be6261564e9ac1c1bd8f92
ahmadx16 <ahmadx16@hotmail.com>
2020-08-10 09:52:36 +0500

    initial prototype

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,5 +5,7 @@ app_name = 'tasks'
 
 urlpatterns = [
     # /tasks/
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('delete/<int:task_id>/', views.delete_task, name='delete-task'),
+    path('toggle/<int:task_id>/', views.toggle_task, name='toggle-task-complete'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,18 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 
 
 def index(request):
     # adding new tasks
-    if request.method =='POST':
-        task = Task (detail=request.POST['newtask'])
+    if request.method == 'POST':
+
+        task = Task(detail=request.POST['newtask'])
         task.save()
-    
+
     all_tasks = Task.objects.all()
     context = {
         "all_tasks": all_tasks
     }
 
-    
-        
-    return render(request, 'tasks/index.html',context)
+    return render(request, 'tasks/index.html', context)
+
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect("tasks:index")
+
+
+def toggle_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.is_complete = not task.is_complete
+    task.save()
+    return redirect("tasks:index")
