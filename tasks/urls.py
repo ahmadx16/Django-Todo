13657f0841fd967709aabab209f9dc227e33f15b
ahmadx16 <ahmadx16@hotmail.com>
2020-08-19 21:25:32 +0500

    UI update + initial working flow

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,6 +6,8 @@ app_name = 'tasks'
 urlpatterns = [
     # /tasks/
     path('', views.index, name='index'),
+    path('add/', views.add, name='add'),
+    path('update/<int:task_id>', views.update_task, name='update-task'),
     path('delete/<int:task_id>/', views.delete_task, name='delete-task'),
     path('toggle/<int:task_id>/', views.toggle_task, name='toggle-task-complete'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -3,11 +3,7 @@ from .models import Task
 
 
 def index(request):
-    # adding new tasks
-    if request.method == 'POST':
-
-        task = Task(detail=request.POST['newtask'])
-        task.save()
+    """View all tasks"""
 
     all_tasks = Task.objects.all()
     context = {
@@ -17,14 +13,45 @@ def index(request):
     return render(request, 'tasks/index.html', context)
 
 
+def add(request):
+    """Adding new tasks"""
+
+    if request.method == 'POST':
+        task = Task(detail=request.POST['newtask'])
+        task.save()
+    return redirect("tasks:index")
+
+
 def delete_task(request, task_id):
+    """Delete task on given task_id"""
+
     task = get_object_or_404(Task, id=task_id)
     task.delete()
     return redirect("tasks:index")
 
 
 def toggle_task(request, task_id):
+    """Toggle complete or incomplete of given id"""
+
     task = get_object_or_404(Task, id=task_id)
     task.is_complete = not task.is_complete
     task.save()
     return redirect("tasks:index")
+
+
+def update_task(request, task_id):
+    """Updates taskof given task_id"""
+
+    if request.method == 'POST':
+        task = get_object_or_404(Task, id=task_id)
+        task.detail = request.POST['updated_task']
+        task.save()
+        return redirect("tasks:index")
+
+    all_tasks = Task.objects.all()
+    context = {
+        "all_tasks": all_tasks,
+        "edit_task": task_id
+    }
+
+    return render(request, 'tasks/index.html', context)
